9c17333d11d403e0147f8bd2f29782e82d2006a3
IvanCS719 <ivancupil17@gmail.com>
2024-06-26 19:48:00 -0600

    Agregando @JsonIgnoreProperties en DatosAutor para traer solo los datos necesarios de la API

--- src/main/java/com/ivandorid/literalura/models/DatosAutor.java
+++ src/main/java/com/ivandorid/literalura/models/DatosAutor.java
@@ -1,8 +1,10 @@
 package com.ivandorid.literalura.models;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 //Deserializar los datos del Autor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosAutor(
     @JsonAlias("name") String nombreAutor,
     @JsonAlias("birth_year") Integer anio_nacimiento,
